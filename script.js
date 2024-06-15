8d59d26d7cf93f1d4efe36798e6014df9142f9d1
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-15 17:58:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 // complete this js code
-// function Person(name, age) {}
-
-// function Employee(name, age, jobTitle) {}
-
-class Person{
-	constructor(name,age){
-		this._name=name;
-		this._age=age;
-	}
-
-	greet(){
-		console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`)
-	}
+function Person(name, age) {
+	this._name=name;
+	this._age=age;
 }
 
-class Employee extends Person{
-	constructor(name,age,jobTitle){
-		super(name,age);
-		this._jobTitle=jobTitle;
-	}
-
-	jobGreet(){
-		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
-	}
+Person.prototype.greet=function(){
+	console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`)
 }
+
+function Employee(name, age, jobTitle) {
+	// like super
+	Person.call(this,name,age)
+	this._jobTitle=jobTitle;
+}
+
+// Add methods of Person constructor fxn which are in Person prototype object 
+Employee.prototype={...Person.prototype}
+
+Employee.prototype.jobGreet=function(){
+	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
+}
+
+// class Person{
+// 	constructor(name,age){
+// 		this._name=name;
+// 		this._age=age;
+// 	}
+
+// 	greet(){
+// 		console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`)
+// 	}
+// }
+
+// class Employee extends Person{
+// 	constructor(name,age,jobTitle){
+// 		super(name,age);
+// 		this._jobTitle=jobTitle;
+// 	}
+
+// 	jobGreet(){
+// 		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
+// 	}
+// }
 
 // const person=new Person("Alice",25)
 // person.greet()
